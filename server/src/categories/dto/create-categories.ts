544b95fe41e1a10295b1dc3e99d060d98342692d
Alexander Enin <enin02@mail.ru>
2025-05-18 16:25:42 +0500

    feat: Добавляет валидацию для создания категорий

--- server/src/categories/dto/create-categories.ts
+++ server/src/categories/dto/create-categories.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateСategoryDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
